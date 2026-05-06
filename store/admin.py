ebffb3304e2af4d5ff4c759e4a61b2c43aa89915
solo3li <hamed3alii.3@gmail.com>
2026-05-05 21:24:37 -0700

    admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Product, ProductSize, Cart, CartItem, Order, OrderItem
 
-# Register your models here.
+class ProductSizeInline(admin.TabularInline):
+    model = ProductSize
+    extra = 1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created_at')
+    inlines = [ProductSizeInline]
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'session_id', 'created_at')
+    inlines = [CartItemInline]
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'total_price', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    inlines = [OrderItemInline]
+
+admin.site.register(CartItem)
+admin.site.register(OrderItem)
+admin.site.register(ProductSize)
